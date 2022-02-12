859ade001088c5c17f296e771cc999e681322196
kubilaybee <kubilaybee@gmail.com>
2022-02-12 17:10:25 +0300

    added the comment line

--- kodluyoruz-frontend-101-egitimi-main/data-algorithms/insertionSort.js
+++ kodluyoruz-frontend-101-egitimi-main/data-algorithms/insertionSort.js
@@ -1,15 +1,20 @@
 let firstArr = [22, 27, 16, 2, 18, 6];
 
 function insertionSort(arr) {
-  let length = arr.length;
+  let arrL = arr.length;
 
-  for (let i = 1; i < length; i++) {
+  for (let i = 1; i < arrL; i++) {
+    // keep the data valuee
     let currentData = arr[i];
+    // get index number of previous data
     let j = i - 1;
+    // compare the values of the dataset efore the current retained value
     while (j > -1 && currentData < arr[j]) {
+      // edit dataset up to current index if conditions are met
       arr[j + 1] = arr[j];
       j--;
     }
+    // assign current value to index
     arr[j + 1] = currentData;
     console.log(arr);
   }
