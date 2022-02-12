06773fb5c314b3f6ead82b74792b147cc4ba8616
kubilaybee <kubilaybee@gmail.com>
2022-02-12 17:02:33 +0300

    fixed algorithm

--- kodluyoruz-frontend-101-egitimi-main/data-algorithms/insertionSort.js
+++ kodluyoruz-frontend-101-egitimi-main/data-algorithms/insertionSort.js
@@ -1,17 +1,17 @@
 let firstArr = [22, 27, 16, 2, 18, 6];
 
 function insertionSort(arr) {
-  let arrLength = arr.length;
+  let length = arr.length;
 
-  for (let i = 1; i < arrLength; i++) {
-    for (let j = 0; j < i; j++) {
-      if (arr[i] < arr[j]) {
-        let tempData = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tempData;
-      }
-      console.log(arr);
+  for (let i = 1; i < length; i++) {
+    let currentData = arr[i];
+    let j = i - 1;
+    while (j > -1 && currentData < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = currentData;
+    console.log(arr);
   }
   return arr;
 }
