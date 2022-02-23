475a8b12f7fdd62c9f3f5ca7f279f93db6a6293e
kubilaybee <kubilaybee@gmail.com>
2022-02-23 20:27:56 +0300

    filter exercise

--- kodluyoruz-frontend-101-egitimi-main/04-javascript-kategorisi/exercise/script.js
+++ kodluyoruz-frontend-101-egitimi-main/04-javascript-kategorisi/exercise/script.js
@@ -6,5 +6,29 @@
 
 // link.innerHTML = "test";
 
-let fullName = prompt("Enter your name");
-console.log(fullName);
+// let fullName = prompt("Enter your name");
+// console.log(fullName);
+
+// filter exercise
+
+const person = [
+  {
+    name: "Adem",
+    age: 25,
+    languages: ["JavaScript", "CSS"],
+  },
+  {
+    name: "Oğuz",
+    age: 33,
+    languages: ["Java", "HTML"],
+  },
+];
+
+// firstTask age bigger than 30
+let firstTask = person.filter((person) => person.age > 30);
+console.log(firstTask);
+// secondTask who know js
+let secondTask = person.filter((person) =>
+  person.languages.includes("JavaScript")
+);
+console.log(secondTask);
